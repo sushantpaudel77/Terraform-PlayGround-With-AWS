9319107d952b522418da5206716e63dae0ec0898
Sushantpaudel77 <sushantpaudel77@gmail.com>
2025-09-19 21:29:14 +0545

    feat add: dynamic variables high to low presidence

---
+++ tf-variables/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.14.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "my-server" {
+  ami           = "ami-043339ea831b48099"
+  instance_type = var.aws_instance_type
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = var.ec2_config.v_size
+    volume_type           = var.ec2_config.v_type
+  }
+
+  tags = merge(var.additional_tags, {
+    Name = "SampleServer"
+  })
+}
